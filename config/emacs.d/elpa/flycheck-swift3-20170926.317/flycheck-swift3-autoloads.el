f508f4b1d049e1bddcb8264ee6345f6e9e82933d
inj0h <14225024+inj0h@users.noreply.github.com>
2017-10-22 23:24:50 -0500

    Change comment in Xresources; reorganize files (cf. log).

---
+++ config/emacs.d/elpa/flycheck-swift3-20170926.317/flycheck-swift3-autoloads.el
@@ -0,0 +1,22 @@
+;;; flycheck-swift3-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "flycheck-swift3" "flycheck-swift3.el" (23009
+;;;;;;  33332 825799 185000))
+;;; Generated autoloads from flycheck-swift3.el
+
+(autoload 'flycheck-swift3-setup "flycheck-swift3" "\
+Set up Flycheck for Swift.
+
+\(fn)" nil nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flycheck-swift3-autoloads.el ends here
---
+++ config/emacs.d/elpa/flycheck-swift3-20170926.317/flycheck-swift3-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-swift3" "20170926.317" "Flycheck: Swift support for Apple swift-mode" '((emacs "24.4") (flycheck "26")) :commit "756833425f51baa9eb0a2fa7493df6e68612c88d" :url "https://github.com/GyazSquare/flycheck-swift3" :keywords '("convenience" "languages" "tools"))
