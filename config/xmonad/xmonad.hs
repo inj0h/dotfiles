89b769fdb964523900f5810ead5e8a921b4da92f
inj0h <14225024+inj0h@users.noreply.github.com>
2018-02-18 02:35:05 -0600

    Add xmonad config (~stable).

---
+++ config/xmonad/xmonad.hs
@@ -0,0 +1,42 @@
+import System.IO
+import XMonad
+import XMonad.Actions.CycleWS
+import XMonad.Actions.SpawnOn
+import XMonad.Config.Gnome
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Accordion
+import XMonad.Layout.Named
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Tabbed
+import XMonad.Prompt
+import XMonad.Prompt.Shell
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.SpawnOnce
+
+myManageHook = composeAll (
+    [ manageHook gnomeConfig
+    , className =? "Unity-2d-panel" --> doIgnore
+    , className =? "Unity-2d-launcher" --> doIgnore
+    , className =? "Audacity" --> doFloat
+    , className =? "Gimp" --> doFloat
+    , manageDocks
+    ])
+
+myStartupHook = do
+    spawnOnce "unity-settings-daemon"
+    spawnOnce "gnome-settings-daemon"
+
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/eric/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = myManageHook
+        , layoutHook = avoidStruts $ smartBorders $ layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                    { ppOutput = hPutStrLn xmproc
+                    , ppTitle = xmobarColor "green" "" . shorten 50
+                    }
+        , startupHook = myStartupHook
+        , terminal    = "gnome-terminal"
+        }
