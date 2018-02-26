91e0d3972b46b4a58627a527e43d991770c512d3
inj0h <14225024+inj0h@users.noreply.github.com>
2018-02-25 21:48:13 -0600

    Update zsh files, cf diff.

--- bin/zfunctions.sh
+++ bin/zfunctions.sh
@@ -8,23 +8,44 @@
 
 # cat then pipe to clipboard
 if [[ `uname` == 'Darwin' ]]; then
-    function catc { cat $1 | pbcopy }
+    catc() {
+        cat $1 | pbcopy
+    }
 else
-    function catc { cat $1 | xclip -selection clipboard }
+    catc() {
+        cat $1 | xclip -selection clipboard
+    }
 fi
 
 # cat then pipe to less
-function catl { cat $1 | less }
+function catl {
+    cat $1 | less -r
+}
 
 # pipe from clipboard
 if [[ `uname` == 'Darwin' ]]; then
-    function cp2 { pbcopy > $1 }
+    function cp2 {
+        pbcopy > $1
+    }
 else
-    function cp2 { xsel -b > $1 }
+    function cp2 {
+        xsel -b > $1
+    }
 fi
 
 # pipe to less
-function pless { $1 | less }
+function pless {
+    $1 | less
+}
 
 # git
-function gshove { git add -A . && git commit -m "Autoupdate." }
+function gshove {
+    git add -A . && git commit -m "Autoupdate."
+}
+
+# sdcv
+if type sdcv >/dev/null 2>&1; then
+    dic() {
+        sdcv -n --color $1 | less -r
+    }
+fi
