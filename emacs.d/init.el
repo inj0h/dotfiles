16774679cde6bcc69addd27a510321d17df97150
inj0h <14225024+inj0h@users.noreply.github.com>
2019-02-05 00:39:32 -0600

    Fix soft tabs in Emacs.
    Use four spaces instead of tabs.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -68,8 +68,7 @@
 ;; Tabs = spaces * 4
 (setq-default indent-tabs-mode nil)
 (setq c-basic-offset 4)
-(setq tab-stop-list (number-sequence 8 120 8))
-(setq tab-width 4)
+(setq-default tab-width 4)
 (setq backward-delete-char-untabify-method 'hungry)
 
 ;; Cleanup trailing whitespace, et al after write
