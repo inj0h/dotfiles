bdff6bd6734cc371fce9fe097abea685779fa5df
inj0h <14225024+inj0h@users.noreply.github.com>
2019-02-07 02:44:39 -0600

    Remap capslock to control in Linux.
    And remove GTK Emacs keys.

--- bash.d/keybindings.bash
+++ bash.d/keybindings.bash
@@ -2,13 +2,8 @@
 # Description: Manage keybindings on Linux.
 #
 
-GTK_SETTINGS="$HOME/.config/gtk-3.0/settings.ini"
-OS="Ubuntu"
-KEYS="Emacs"
-
-if [[ `uname -s` == 'Linux' ]]; then
-    DISTRO=$(lsb_release -i | cut -f 2-)
-    if [ $DISTRO == $OS ]; then
-        gsettings set org.gnome.desktop.interface gtk-key-theme $KEYS
-    fi
-fi
+#
+# Linux
+#
+# Remap capslock to control.
+[[ `uname -s` == 'Linux' ]] && setxkbmap -option caps:ctrl_modifier
