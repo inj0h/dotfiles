5df06bebc1373dbc9dae8e83447b0806befab095
inj0h <14225024+inj0h@users.noreply.github.com>
2019-07-10 03:23:14 -0500

    Remove X keybindings shell settings.

--- lib/keybindings.bash
+++
@@ -1,9 +0,0 @@
-# Filename: keybindings.bash
-# Note:     Manage keybindings on Linux.
-#
-
-#
-# Linux
-#
-# Remap capslock to control.
-[[ `uname -s` == 'Linux' ]] && setxkbmap -option caps:ctrl_modifier
