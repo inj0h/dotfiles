120bdad7eb49413296352ec120cb169364ebd6ed
inj0h <14225024+inj0h@users.noreply.github.com>
2019-07-31 00:27:52 -0500

    Remove rainbow-delimiters custom colors.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -5,18 +5,4 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (org-bullets exec-path-from-shell deadgrep smex flx yasnippet web-mode gitignore-mode markdown-mode haskell-mode flycheck rainbow-delimiters counsel-projectile projectile company counsel ivy evil-magit evil-commentary evil-surround evil-leader evil-escape evil fill-column-indicator abyss-theme which-key diminish use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(rainbow-delimiters-depth-1-face ((t (:inherit rainbow-delimiters-base-face :foreground "#ae81ff"))))
- '(rainbow-delimiters-depth-2-face ((t (:inherit rainbow-delimiters-base-face :foreground "#66d9ef"))))
- '(rainbow-delimiters-depth-3-face ((t (:inherit rainbow-delimiters-base-face :foreground "#a6e22e"))))
- '(rainbow-delimiters-depth-4-face ((t (:inherit rainbow-delimiters-base-face :foreground "#e6db74"))))
- '(rainbow-delimiters-depth-5-face ((t (:inherit rainbow-delimiters-base-face :foreground "#fd971f"))))
- '(rainbow-delimiters-depth-6-face ((t (:inherit rainbow-delimiters-base-face :foreground "#f92672"))))
- '(rainbow-delimiters-depth-7-face ((t (:inherit rainbow-delimiters-base-face :foreground "#ae81ff"))))
- '(rainbow-delimiters-depth-8-face ((t (:inherit rainbow-delimiters-base-face :foreground "#66d9ef"))))
- '(rainbow-delimiters-depth-9-face ((t (:inherit rainbow-delimiters-base-face :foreground "#a6e22e")))))
+    (nord-theme org-bullets exec-path-from-shell deadgrep smex flx yasnippet web-mode gitignore-mode markdown-mode haskell-mode flycheck rainbow-delimiters counsel-projectile projectile company counsel ivy evil-magit evil-commentary evil-surround evil-leader evil-escape evil fill-column-indicator abyss-theme which-key diminish use-package))))
