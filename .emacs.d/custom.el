f2bcdca688f46dca6f0c8afeadfcda305e0f62bb
inj0h <14225024+inj0h@users.noreply.github.com>
2019-09-13 01:08:10 -0500

    Fix Emacs directory name.
    Add the proper dot.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (evil-magit evil-commentary evil-surround evil-leader evil-escape undo-tree yasnippet web-mode org-bullets markdown-mode gitignore-mode git-commit haskell-mode flycheck rainbow-delimiters counsel-projectile projectile company deadgrep smex counsel ivy evil dracula-theme which-key exec-path-from-shell diminish use-package))))
+    (web-mode org-bullets markdown-mode gitignore-mode haskell-mode flycheck rainbow-delimiters company projectile deadgrep smex evil-surround evil-magit evil-leader evil-escape evil-commentary evil dracula-theme which-key exec-path-from-shell diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
