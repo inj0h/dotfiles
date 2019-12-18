9a66b2294469c4047ce4e8f4002b5653ca3424d9
inj0h <14225024+inj0h@users.noreply.github.com>
2019-12-17 20:08:49 -0600

    Remove minor whitespace in dotemacs.org.
    Also, trim header comment in init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,8 +2,7 @@
 ;;
 ;; Filename: init.el
 ;; Note:     A minimal Emacs initialization file to bootstrap an Org
-;;           Mode configuration using Org Babel.
-;;
+;;           configuration using Org Babel.
 
 ;; Load the Org configuration at the specified path.
 (org-babel-load-file "~/.emacs.d/dotemacs.org")
